a4725fb45d934298fedba4d100e8d43ba76e7e85
theRhasoldy <rhasoldy@gmail.com>
2022-08-26 17:36:52 +0000

    Done :)

--- lib/reservation.dart
+++ lib/reservation.dart
@@ -2,22 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-class MyItem {
-  bool isExpanded;
-  final String header;
-  final String body;
-  final AssetImage image;
-  final String value;
-  final double rating;
-  MyItem(
-      {this.isExpanded = false,
-      required this.header,
-      required this.body,
-      required this.image,
-      required this.value,
-      required this.rating});
-}
-
 class Reservations extends StatefulWidget {
   final DateTime inDate;
   final DateTime outDate;
@@ -40,104 +24,204 @@ class Reservations extends StatefulWidget {
 }
 
 class _ReservationsState extends State<Reservations> {
-  bool isSelected = false;
-  String room = "";
+  bool singleSelected = false;
+  bool doubleSelected = false;
+  bool suiteSelected = false;
+  bool singleExpanded = false;
+  bool doubleExpanded = false;
+  bool suiteSExpanded = false;
+  String room = "none";
 
-  final List<MyItem> _itmes = <MyItem>[
-    MyItem(
-        image: const AssetImage("images/resort-hero.jpg"),
-        header: "Single Room",
-        body: "A room assigned to one person",
-        value: "single",
-        rating: 3),
-    MyItem(
-        image: const AssetImage("images/resort-hero.jpg"),
-        header: "Double Room",
-        body: "A room assigned to two people. May have one or more beds.",
-        value: "double",
-        rating: 4),
-    MyItem(
-        image: const AssetImage("images/resort-hero.jpg"),
-        header: "Suite Room",
-        body: "A room with one or more bedrooms and a seperate living space",
-        value: "suite",
-        rating: 5),
-  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: "title",
         home: Scaffold(
-            appBar: AppBar(
-              title: const Text(
-                "Castaway Resort",
-                style: TextStyle(fontFamily: "Serif"),
-              ),
-              backgroundColor: const Color(0xff00BCD4),
-              actions: [
-                IconButton(
-                    onPressed: () => Navigator.pop(context),
-                    icon: const Icon(Icons.arrow_back))
-              ],
+          appBar: AppBar(
+            title: const Text(
+              "Castaway Resort",
+              style: TextStyle(fontFamily: "Serif"),
             ),
-            body: SafeArea(
-                child: Padding(
+            backgroundColor: const Color(0xff00BCD4),
+            actions: [
+              IconButton(
+                  onPressed: () => Navigator.pop(context),
+                  icon: const Icon(Icons.arrow_back))
+            ],
+          ),
+          body: SafeArea(
+            child: Padding(
               padding: const EdgeInsets.all(8),
               child: ListView(
-                children: <Widget>[
+                children: [
                   ExpansionPanelList(
-                    expansionCallback: (int index, bool isExpanded) {
-                      setState(() {
-                        _itmes[index].isExpanded = !_itmes[index].isExpanded;
-                      });
-                    },
-                    children: _itmes.map<ExpansionPanel>((MyItem item) {
-                      return ExpansionPanel(
-                        headerBuilder: (BuildContext context, bool isExpanded) {
-                          return ListTile(
-                            title: Text(
-                              item.header,
-                              style: const TextStyle(
-                                  color: Color(0xff008D9F),
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 12),
+                      expansionCallback: (int index, bool isExpanded) {
+                        setState(() {
+                          singleExpanded = !isExpanded;
+                        });
+                      },
+                      children: [
+                        ExpansionPanel(
+                          headerBuilder:
+                              (BuildContext context, bool isExpanded) {
+                            return ListTile(
+                              title: Text(
+                                "Single Room",
+                                style: const TextStyle(
+                                    color: Color(0xff008D9F),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 12),
+                              ),
+                              leading: Image(
+                                  image: AssetImage("images/resort-hero.jpg")),
+                              trailing: Switch(
+                                  value: singleSelected,
+                                  onChanged: (bool selected) {
+                                    setState(() {
+                                      room = "single";
+                                      singleSelected = selected;
+                                      doubleSelected = false;
+                                      suiteSelected = false;
+                                      print(room);
+                                    });
+                                  }),
+                            );
+                          },
+                          body: ListTile(
+                            leading: SizedBox(
+                              width: 100,
+                              child: RatingBar(
+                                  itemSize: 20,
+                                  initialRating: 4,
+                                  ratingWidget: RatingWidget(
+                                      full: const Icon(
+                                        Icons.star_rate,
+                                        color: Colors.orange,
+                                      ),
+                                      half: const Icon(Icons.star_half),
+                                      empty: const Icon(Icons.star_outline)),
+                                  onRatingUpdate: (rating) {}),
                             ),
-                            leading: Image(image: item.image),
-                            trailing: Switch(
-                                value: isSelected,
-                                onChanged: (bool selected) {
-                                  setState(() {
-                                    room = item.header;
-                                    isSelected = selected;
-                                    print(room);
-                                  });
-                                }),
-                          );
-                        },
-                        body: ListTile(
-                          leading: SizedBox(
-                            width: 100,
-                            child: RatingBar(
-                                itemSize: 20,
-                                initialRating: item.rating,
-                                ratingWidget: RatingWidget(
-                                    full: const Icon(
-                                      Icons.star_rate,
-                                      color: Colors.orange,
-                                    ),
-                                    half: const Icon(Icons.star_half),
-                                    empty: const Icon(Icons.star_outline)),
-                                onRatingUpdate: (rating) {}),
+                            title: Text(
+                              "A room assigned for one person",
+                              style: const TextStyle(fontSize: 12),
+                            ),
                           ),
-                          title: Text(
-                            item.body,
-                            style: const TextStyle(fontSize: 12),
+                          isExpanded: singleExpanded,
+                        )
+                      ]),
+                  ExpansionPanelList(
+                      expansionCallback: (int index, bool isExpanded) {
+                        setState(() {
+                          doubleExpanded = !isExpanded;
+                        });
+                      },
+                      children: [
+                        ExpansionPanel(
+                          headerBuilder:
+                              (BuildContext context, bool isExpanded) {
+                            return ListTile(
+                              title: Text(
+                                "Double Room",
+                                style: const TextStyle(
+                                    color: Color(0xff008D9F),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 12),
+                              ),
+                              leading: Image(
+                                  image: AssetImage("images/resort-hero.jpg")),
+                              trailing: Switch(
+                                  value: doubleSelected,
+                                  onChanged: (bool selected) {
+                                    setState(() {
+                                      room = "double";
+                                      doubleSelected = selected;
+                                      singleSelected = false;
+                                      suiteSelected = false;
+                                      print(room);
+                                    });
+                                  }),
+                            );
+                          },
+                          body: ListTile(
+                            leading: SizedBox(
+                              width: 100,
+                              child: RatingBar(
+                                  itemSize: 20,
+                                  initialRating: 4,
+                                  ratingWidget: RatingWidget(
+                                      full: const Icon(
+                                        Icons.star_rate,
+                                        color: Colors.orange,
+                                      ),
+                                      half: const Icon(Icons.star_half),
+                                      empty: const Icon(Icons.star_outline)),
+                                  onRatingUpdate: (rating) {}),
+                            ),
+                            title: Text(
+                              "A room assigned for one person",
+                              style: const TextStyle(fontSize: 12),
+                            ),
                           ),
-                        ),
-                        isExpanded: item.isExpanded,
-                      );
-                    }).toList(),
-                  ),
+                          isExpanded: doubleExpanded,
+                        )
+                      ]),
+                  ExpansionPanelList(
+                      expansionCallback: (int index, bool isExpanded) {
+                        setState(() {
+                          suiteSExpanded = !isExpanded;
+                        });
+                      },
+                      children: [
+                        ExpansionPanel(
+                          headerBuilder:
+                              (BuildContext context, bool isExpanded) {
+                            return ListTile(
+                              title: Text(
+                                "Suite Room",
+                                style: const TextStyle(
+                                    color: Color(0xff008D9F),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 12),
+                              ),
+                              leading: Image(
+                                  image: AssetImage("images/resort-hero.jpg")),
+                              trailing: Switch(
+                                  value: suiteSelected,
+                                  onChanged: (bool selected) {
+                                    setState(() {
+                                      room = "suite";
+                                      suiteSelected = selected;
+                                      singleSelected = false;
+                                      doubleSelected = false;
+                                      print(room);
+                                    });
+                                  }),
+                            );
+                          },
+                          body: ListTile(
+                            leading: SizedBox(
+                              width: 100,
+                              child: RatingBar(
+                                  itemSize: 20,
+                                  initialRating: 4,
+                                  ratingWidget: RatingWidget(
+                                      full: const Icon(
+                                        Icons.star_rate,
+                                        color: Colors.orange,
+                                      ),
+                                      half: const Icon(Icons.star_half),
+                                      empty: const Icon(Icons.star_outline)),
+                                  onRatingUpdate: (rating) {}),
+                            ),
+                            title: Text(
+                              "A room assigned for one person",
+                              style: const TextStyle(fontSize: 12),
+                            ),
+                          ),
+                          isExpanded: suiteSExpanded,
+                        )
+                      ]),
                   TextButton(
                       onPressed: () {
                         reserve(
@@ -146,7 +230,8 @@ class _ReservationsState extends State<Reservations> {
                             children: widget.children,
                             adults: widget.adults,
                             extras: widget.extras,
-                            view: widget.views);
+                            view: widget.views,
+                            room: room);
                       },
                       style: ButtonStyle(
                           backgroundColor: MaterialStateProperty.all(
@@ -157,16 +242,20 @@ class _ReservationsState extends State<Reservations> {
                       )),
                 ],
               ),
-            ))));
+            ),
+          ),
+        ));
   }
 
-  Future reserve(
-      {required DateTime inDate,
-      required DateTime outDate,
-      required double children,
-      required double adults,
-      required String extras,
-      required String view}) async {
+  Future reserve({
+    required DateTime inDate,
+    required DateTime outDate,
+    required double children,
+    required double adults,
+    required String extras,
+    required String view,
+    required String room,
+  }) async {
     final docReservation =
         FirebaseFirestore.instance.collection("reservations").doc();
 
@@ -176,7 +265,8 @@ class _ReservationsState extends State<Reservations> {
       "children": children,
       "adults": adults,
       "extras": extras,
-      "view": view
+      "view": view,
+      "room": room
     };
 
     await docReservation.set(json);
